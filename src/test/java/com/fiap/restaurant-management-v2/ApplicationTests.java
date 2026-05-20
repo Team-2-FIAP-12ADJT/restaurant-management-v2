8dfbc1505eb27706f6d2696302f275451c4afe22
Ricardo Lima <ricardo.engp@gmail.com>
2026-05-19 21:29:43 -0300

    initial commit

---
+++ src/test/java/com/fiap/restaurant-management-v2/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.fiap.restaurant_management_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
